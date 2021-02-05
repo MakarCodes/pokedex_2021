97c7e8c66119b28a2bfba8e8fb4521867aed95a7
MakarCodes <jarek.strupek@wp.pl>
2021-02-05 12:16:16 +0100

    Add modal component

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import styles from './Modal.module.scss';
 
-const useModalLogic = () => {
+export const useModalLogic = () => {
   const [isVisible, setIsVisible] = useState(false);
 
   const toggleVisibility = () => setIsVisible(!isVisible);
@@ -8,8 +10,51 @@ const useModalLogic = () => {
   return { isVisible, toggleVisibility };
 };
 
-const Modal = () => {
-  return <div></div>;
+interface IProps {
+  isVisible: boolean;
+  toggleVisibility: () => void;
+  children?: React.ReactNode;
+}
+
+const Modal: React.FC<IProps> = ({ isVisible, toggleVisibility, children }) => {
+  const handleEscape = useCallback(event => {
+    if (event.keyCode === 27) {
+      toggleVisibility();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isVisible) document.addEventListener('keydown', handleEscape, false);
+    return () => document.removeEventListener('keydown', handleEscape, false);
+  }, [handleEscape, isVisible]);
+
+  return isVisible
+    ? ReactDOM.createPortal(
+        <>
+          <div
+            className={styles.Overlay}
+            onClick={() => toggleVisibility()}
+          ></div>
+          <div className={styles.Wrapper}>
+            <div className={styles.Modal}>
+              <div className={styles.ModalClose}>
+                <button
+                  type='button'
+                  className={styles.ModalCloseButton}
+                  data-dismiss='modal'
+                  aria-label='Close'
+                  onClick={() => toggleVisibility()}
+                >
+                  <span aria-hidden='true'>&times;</span>
+                </button>
+              </div>
+              {children}
+            </div>
+          </div>
+        </>,
+        document.body
+      )
+    : null;
 };
 
 export default Modal;
